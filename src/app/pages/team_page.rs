020d499479adca0588a8c8b2ec25379b3d631889
Candra Aji Ristiawan <candraajiristiawan@gmail.com>
2025-02-12 08:34:32 +0700

    modify team pages

--- src/app/pages/team_page.rs
+++ src/app/pages/team_page.rs
@@ -1,14 +1,35 @@
-use leptos::*;
+use leptos::{ev::MouseEvent, *};
 
 use crate::app::components::Header;
 
 #[component]
 pub fn TeamPage() -> impl IntoView {
+
+   const ADD_BUTTON_STYLE: &str = "bg-[#7734e7] px-8 py-2 rounded text-white
+        transition-all duration-1000 ease-in-out hover:bg-[#8448e9]";
+
+  let (if_show_modal, set_if_show_modal) = create_signal(false);
+
+  let on_click = move |_: MouseEvent| {
+    set_if_show_modal(!if_show_modal());
+  };
+
     view! {
         
       <div class="w-full max-w-[64rem] mx-auto items-center justify-center align-center">
         <Header/>
-        <div class="mt-20 ml-20 text-white">"This is Team Page"</div></div>
+        <div class="mt-20">
+          <div class="text-white flex flex-col w-full mx-auto items-center justify-center z-25">
+            <div class="flex flex-row w-full max-w-[52rem]">
+              <div class="pr-4 mt-4 text-xl">"Members"</div>
+              <hr class="w-full max-w-[48rem] pl-4 pr-4 pt-4 mt-8 mr-4" />
+              <button on:click=on_click clas=ADD_BUTTON_STYLE>
+              "ADD"</button>
+            </div>
+          </div>
+        </div>
+        
+      </div>
        
     }
 }
