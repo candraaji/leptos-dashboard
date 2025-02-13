705b9942e4f04d1dc0f7baaf2ef5c0081a9c5696
Candra Aji Ristiawan <candraajiristiawan@gmail.com>
2025-02-13 20:54:14 +0700

    no error style

--- src/app/components/add_person_modal.rs
+++ src/app/components/add_person_modal.rs
@@ -11,4 +11,8 @@ pub fn AddPersonModal(
     const INPUT_STYLE: &str = "w-full h-23 bg-[#333333] pr-4 pl-6 py-4 text-white mt-6 outline-none focus:outline-none focus:pl-7 transition-all duration-1000 ease-in-out";
 
     const CANCEL_BUTTON_STYLE: &str = "mt-10 bg-[#555555] px-8 py-2 rounded text-white mr-3 transition-all duration-1000 ease-in-out hover:bg-[#666666]";
+
+    const ADD_BUTTON_STYLE: &str = "mt-10 bg-[#7734e7] px-8 py-2 rounded text-white transition-all duration-1000 ease-in-out hover:bg-[#8448e9]";
+
+    const NO_ERROR_STYLE: &str = "flex flex-col bg-[#222222] border-t-8 border-[#7734e7] px-6 pt-5 h-[29rem] w-full max-w-[36rem] z-50 -mt-2 fixed z-50";
 }
