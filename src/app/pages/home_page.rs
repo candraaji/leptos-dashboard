e83e9adecc95043ea4c1a8c906d7ae8ac12d3ca4
Candra Aji Ristiawan <candraajiristiawan@gmail.com>
2025-02-15 13:18:39 +0700

    add component header dashboard

--- src/app/pages/home_page.rs
+++ src/app/pages/home_page.rs
@@ -1,13 +1,18 @@
 use leptos::*;
-use crate::app::components::Header;
+use crate::app::components::{Header, DashboardHeader};
+use crate::app::server_functions::get_persons;
 
 #[component]
 pub fn HomePage() -> impl IntoView {
+
+    let get_persons_info = create_resource(|| (), |_|{
+      async move { get_persons().await }
+    });
+
     view! {
       <div class="w-full max-w-[64rem] mx-auto items-center justify-center align-center">
           <Header/>
-          <div class="mt-20 ml-20 text-white">"Welcome to Dashboard"</div>
-         
+          <DashboardHeader />
       </div>
         
     }
